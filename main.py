f972c30fd1de72f97421fa32f4de34c51dd72a8f
Ariya _ <heisen@definitely-not-windows.local>
2022-07-07 16:49:03 +0900

    JSON writes directly to HTML SVG tag

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from asyncio.windows_events import NULL
-from inspect import _void
 import os
 import json
 import numpy as np
-import scipy.interpolate
 import cv2
 from matplotlib import pyplot as plt
 from PIL import Image as im
@@ -44,6 +41,23 @@ def run_potrace(IMG_NAME: str, seed: int):
 
 def parse_json(IMG_NAME: str, seed: int):
 
+    output_html = open(f'{IMG_NAME}_index.html', 'w')
+    output_html.write(f'''
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <meta name="Description" content="{IMG_NAME}_{seed}"/>
+            <link rel="stylesheet" href="base.css">
+            <title>{IMG_NAME}_{seed}</title>
+        </head>
+        <body>
+            <div class="page">
+                <svg height="100%" width="100%">\n
+    ''')
+
     with open(f'{IMG_NAME}_{seed}.json', 'r') as fl:
         json_text = json.load(fl)
 
@@ -51,20 +65,22 @@ def parse_json(IMG_NAME: str, seed: int):
             coords = ucoord['geometry']['coordinates'][0]
 
             for idx in range(0, len(coords) - 3, 3):
-                base_string = '<path d="'
-                fin_string = '" stroke="white" stroke-width="1" fill="none" />'
+                base_string: str = '<path d="'
+                fin_string: str = '" stroke="white" stroke-width="1" fill="none" />'
 
                 base_string += f'M {coords[idx][0]} {coords[idx][1]} S {coords[idx + 1][0]} {coords[idx + 1][1]} {coords[idx + 2][0]} {coords[idx + 2][1]}' + fin_string
+                output_html.writelines([base_string])
                 print(base_string)
 
+            # Don't remove this
             break
 
-        plt.show()
+    output_html.write('</svg></div></body></html>')
 
 
 if __name__ == '__main__':
 
-    IMG_NAME = 'test_image'
+    IMG_NAME = 'test_image_2'
     IMG_EXTENSION = 'jpeg'
 
     seed = image_to_vector(IMG_NAME, IMG_EXTENSION)
